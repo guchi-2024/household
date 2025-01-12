7005e0edbfa9ecac6140787c6fa5e95f4a3ea016
guchi-2024 <163069131+guchi-2024@users.noreply.github.com>
2025-01-12 20:57:58 +0900

    Outletを使ってRouteを振り分ける

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Report from './pages/Report'
 import NoMactch from './pages/NoMactch'
+import AppLayout from './components/layout/AppLayout'
 
 
 function App() {
   return (
    <Router>
     <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/report" element={<Report />}/>
-      <Route path="/*" element={<NoMactch />}/>
+      <Route path="/" element={<AppLayout />}>
+        <Route path="/" element={<Home />}/>
+        <Route path="/report" element={<Report />}/>
+        <Route path="/*" element={<NoMactch />}/>
+      </Route>
     </Routes>
    </Router>
   )
